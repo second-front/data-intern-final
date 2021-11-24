2500137712991c300f26936db4096adfc37eb095
Kyle Orciuch <korciuch@stanford.edu>
2021-11-24 07:21:44 -0800

    notebooks are slow

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 import xgboost as xgb
 import matplotlib.pyplot  as plt
 from pymongo import MongoClient
+from scipy.sparse import csr_matrix
+from scipy import sparse
 
 DB_CONNECTION = 'mongodb://localhost:27017'
 
@@ -16,49 +18,67 @@ def train_xgb_model(vocabulary_src):
     max_id = int(last_id)
     client = MongoClient(DB_CONNECTION)
     training_examples = []
+    training_labels = []
     validation_examples = []
+    validation_labels = []
+    counter = 0
     for example in client.data.norm.train.vectorized.find():
-        v = np.zeros((max_id+2,), dtype=int)
+        v = np.zeros((max_id+1,), dtype=float)
         for id,val in example['features']: v[id] = val # X_j^(i)
-        v[max_id+1] = int(example['label']) # Y^(i)
-        training_examples.append(v)
-        print(sys.getsizeof(training_examples))
-    D = np.asarray(training_examples,dtype=int)
-    X_train = D[:,0:max_id+1]
-    Y_train = D[:,max_id+1]
+        v = np.divide(v,np.max(v))
+        training_examples.append(csr_matrix(v))
+        training_labels.append(int(example['label']))
+        counter += 1
+        print(counter)
+    X_train = sparse.vstack(training_examples)
+    Y_train = np.asarray(training_labels)
+    dtrain = xgb.DMatrix(X_train,label=Y_train)
     print('done querying train...')
+    counter = 0
     for example in client.data.norm.test.vectorized.find():
-        v = np.zeros((max_id+2,), dtype=int)
+        v = np.zeros((max_id+1,), dtype=float)
         for id,val in example['features']: v[id] = val # X_j^(i)
-        v[max_id+1] = int(example['label']) # Y^(i)
-        validation_examples.append(v)
-    D = np.asarray(validation_examples,dtype=int)
-    X_test = D[:,0:max_id+1]
-    Y_test = D[:,max_id+1]
+        v = np.divide(v,np.max(v))
+        validation_examples.append(csr_matrix(v))
+        validation_labels.append(int(example['label']))
+        counter += 1
+        print(counter)
+    X_test = sparse.vstack(validation_examples)
+    Y_test = np.asarray(validation_labels)
+    dtest = xgb.DMatrix(X_test,label=Y_test)
     client.close()
+    eval_metrics = ['error', 'logloss']
+    eval_set = [(X_train, Y_train),(X_test, Y_test)]
+    model = xgb.XGBClassifier(objective='binary:logistic',n_estimators=1500,max_depth=6,learning_rate=0.1)
+    model.fit(X_train,Y_train,eval_set=eval_set,eval_metric=eval_metrics,verbose=True)
+    pickle.dump(model, open("pima.pickle.dat", "wb"))
+    res = model.evals_result()
+    """
     print('done querying test...')
-    dtrain = xgb.DMatrix(X_train, label=Y_train)
-    dtest = xgb.DMatrix(X_test, label=Y_test)
-    param = {
+    params = {
         'objective': 'binary:logistic', # Specify binary classification
         'tree_method': 'gpu_hist', # Use GPU accelerated algorithm
-        'eval_metric': ['error', 'logloss']
+        'eval_metric': ['error', 'logloss'],
+        'num_boost_round': 100,
+        'early_stopping_rounds': 10
     }
     gpu_res = {}
     tmp = time.time()
-    model = xgb.train(param,dtrain,evals=[(dtrain,'train'),(dtest,'test')],evals_result=gpu_res,verbose_eval=True)
+    model = xgb.train(params,dtrain,evals=[(dtrain,'train'),(dtest,'test')],evals_result=gpu_res,verbose_eval=True)
     pickle.dump(model, open("pima.pickle.dat", "wb"))
     print("GPU Training Time: %s seconds" % (str(time.time() - tmp)))
     print(gpu_res)
     return gpu_res
+    """
+    return res
 
 def plot_loss(results_dict, output_path, show_plot):    
-    epochs = len(results_dict['train']['error'])
+    epochs = len(results_dict['validation_0']['error'])
     x_axis = range(0, epochs)
     # plot log loss
     fig = plt.figure(figsize=(7,5))
-    plt.plot(x_axis, results_dict['train']['logloss'], label='Train')
-    plt.plot(x_axis, results_dict['test']['logloss'], label='Test')
+    plt.plot(x_axis, results_dict['validation_0']['logloss'], label='Train')
+    plt.plot(x_axis, results_dict['validation_1']['logloss'], label='Test')
     plt.legend()
     plt.ylabel('Log Loss')
     plt.xlabel('Epochs')
@@ -69,12 +89,12 @@ def plot_loss(results_dict, output_path, show_plot):
 
 def plot_error(results_dict, output_path, show_plot):        
     # plot classification error
-    epochs = len(results_dict['train']['error'])
+    epochs = len(results_dict['validation_0']['error'])
     x_axis = range(0, epochs)
     # plot error
     fig = plt.figure(figsize=(7,5))
-    plt.plot(x_axis, results_dict['train']['error'], label='Train')
-    plt.plot(x_axis, results_dict['test']['error'], label='Test')
+    plt.plot(x_axis, results_dict['validation_0']['error'], label='Train')
+    plt.plot(x_axis, results_dict['validation_1']['error'], label='Test')
     plt.legend()
     plt.ylabel('Error')
     plt.xlabel('Epochs')
@@ -83,39 +103,38 @@ def plot_error(results_dict, output_path, show_plot):
     if show_plot==True:
         plt.show()
 
-def extract_split(example_list, max_id):
-    data = []
-    for example in example_list[:int(len(example_list))]:
-        v = np.zeros((max_id+2,), dtype=np.int64)
-        for id,val in example['features']: v[id] = val # X_j^(i)
-        v[max_id+1] = int(example['label']) # Y^(i)
-        data.append(v)
-    D = np.asarray(data,dtype=np.int64)
-    X = D[:,0:max_id+1]
-    Y = D[:,max_id+1]
-    return (X,Y)
-
-def predict(model_src):
+def predict(model):
     client = MongoClient(DB_CONNECTION)
     validation_examples = []
-    for example in client.data.norm.test.vectorized.find():
-        validation_examples.append(example)
+    validation_labels = []
     with open("./vocabularies/vocabulary_100k_docfreq_100_hq.csv") as f:
         for line in f: pass
         last_line = line
-        id = last_line.split(',')[0]
-    loaded_model = pickle.load(open(model_src, "rb"))
-    X_test, Y_test = extract_split(example_list=validation_examples,max_id=int(id))
-    dtest = xgb.DMatrix(X_test, label=Y_test)
-    Y_pred = loaded_model.predict(dtest)
+        last_id = last_line.split(',')[0]
+    max_id = int(last_id)
+    counter = 1
+    for example in client.data.norm.test.vectorized.find():
+        v = np.zeros((max_id+1,), dtype=float)
+        for id,val in example['features']: v[id] = val # X_j^(i)
+        v = np.divide(v,np.max(v))
+        validation_examples.append(csr_matrix(v))
+        validation_labels.append(int(example['label']))
+        counter += 1
+        print(counter)
+    X_test = sparse.vstack(validation_examples)
+    Y_test = np.asarray(validation_labels)
+    Y_pred = model.predict(X_test)
     print(Y_pred)
     print(Y_test)
     accuracy = sum([1/len(Y_pred) * (round(i) == j) for i,j in zip(Y_pred,Y_test)])
     print(accuracy)
 
 if __name__ == '__main__':
-    res = train_xgb_model(vocabulary_src='./vocabularies/vocabulary_100k_docfreq_100_hq.csv')
-    plot_loss(res, output_path="log-loss-init.jpg", show_plot=False)
-    plot_error(res, output_path="error-init.jpg", show_plot=False)
-    predict('pima.pickle.dat')
-
+    #res = train_xgb_model(vocabulary_src='./vocabularies/vocabulary_100k_docfreq_100_hq.csv')
+    loaded_model = pickle.load(open('pima.pickle.dat', "rb"))
+    print(loaded_model)
+    res = loaded_model.evals_result()
+    print(res)
+    plot_loss(res, output_path="log-loss-init.png", show_plot=True)
+    plot_error(res, output_path="error-init.png", show_plot=True)
+    predict(loaded_model)
---
+++ scrape.py
@@ -0,0 +1,210 @@
+import csv
+import json
+import requests
+import datetime
+import regex as re
+import numpy as np
+import headers as h
+import concurrent.futures
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+DB_CONNECTION = 'mongodb://localhost:27017'
+
+def url_match(data, link) -> str:
+    if len(link.attrs['href']) == 0: return ''
+    match = re.search(h.DIRECT_SUB_DOMAINS, link.attrs['href'])
+    new_target = ""
+    sep = ''
+    if match:
+        new_target = link.attrs['href']
+    elif "." not in link.attrs['href'] and is_entrypoint(link.attrs['href']):
+        print("RELATIVE MATCH (SITEMAP): ", link.attrs['href'])
+        if link.attrs['href'][0] != '/': sep = '/'
+        new_target = data[h.DOMAIN] + sep + link.attrs['href']
+    elif "." not in link.attrs['href'] and is_entrypoint(data[h.DOMAIN]):
+        print("RELATIVE MATCH (INTRA-SITEMAP): ", link.attrs['href'])
+        base_match = re.search("^([^\/]+)\/", data[h.DOMAIN])
+        if base_match != None:
+            base = base_match.group(1)
+        if link.attrs['href'][0] != '/': sep = '/'
+        new_target = base + sep + link.attrs['href']
+    elif "." not in link.attrs['href']:
+        print("RELATIVE MATCH (NON-SITEMAP): ", link.attrs['href'])
+        if link.attrs['href'][0] != '/': sep = '/'
+        new_target = data[h.DOMAIN] + sep + link.attrs['href']
+    return new_target
+
+def find_about_pages(links, data):
+    href_list = []
+    filtered_links = []
+    for link in links: 
+        if 'href' in link.attrs.keys():
+            char_set = set(link.attrs['href'])
+            if char_set.isdisjoint({'#','(',')','@'}) and '.pdf' not in link.attrs['href']:
+                href_list.append(link.attrs['href'])
+                filtered_links.append(link)
+    entry = {
+        'id': data[h.ID],
+        'origin': complete_url(data[h.DOMAIN]),
+        'all_link_count': len(links),
+        'links': str(href_list),
+    }
+    client = MongoClient(DB_CONNECTION)
+    client.data.sublinks.insert_one(entry)
+    client.close()
+
+    selected_urls = None
+    if len(filtered_links) > h.MAX_SUBPAGES:
+        selected_urls = []
+        indices = np.random.choice(len(filtered_links), h.MAX_SUBPAGES, replace=False)
+        for index in indices:
+            res = url_match(data, filtered_links[index])
+            if res != "" and re.search(h.DIRECT_SUB_DOMAINS, complete_url(res)):
+                selected_urls.append(filtered_links[index])
+    else: selected_urls = filtered_links
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_url = {
+            executor.submit(fetch_html, data, url_match(data, link)): \
+                link for link in selected_urls
+        }
+
+def extract_text(tags):
+    texts = []
+    for t in tags:
+        inner_match = re.search("<.*>([\w -,.!?\"\']+)<.*>", str(t))
+        if inner_match:
+            text = inner_match.group(1)
+            texts.append(text.replace("\"", '\''))
+    text = ' '.join(texts)
+    if len(text) < h.LEN_CUTOFF: return None
+    else: return text
+
+def complete_url(base_url) -> str:
+    if 'https://www.' not in base_url: 
+        return 'https://www.' + base_url
+    else: return base_url
+
+def is_entrypoint(href) -> bool:
+    for elem in h.ENTRYPOINT_POSITIVES:
+        if elem in href:
+            return True
+
+def find_site_map(links, data) -> list:
+    site_map_targets = []
+    for link in links:
+        if 'href' in link.attrs.keys() and is_entrypoint(link.attrs['href']):
+            new_target = url_match(data, link)
+            if new_target != "":
+                site_map_targets.append(new_target)
+    return site_map_targets
+
+def fetch_html(data, base_url):
+    print('base_url:', base_url)
+    error = ''
+    if base_url == '': return None
+    full_url = complete_url(base_url)
+    try:
+        result = requests.get(full_url, timeout=h.TIMEOUT, headers=np.random.choice(h.HEADERS))
+        status = result.status_code
+        print("URL: ", full_url)
+        print("STATUS: ", status)
+        src = result.content
+        soup = BeautifulSoup(src, 'html.parser')
+        html_tags = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+        text = extract_text(html_tags)
+
+        if result.status_code != 200: text = u''
+        elif result.status_code == 200:
+            document = {
+                'id': data[h.ID],
+                'endpoint': full_url,
+                'status': result.status_code,
+                'time': datetime.datetime.now(),
+                'text': text
+            }
+            client = MongoClient(DB_CONNECTION)
+            if not client.data.companies.find_one({'id': data[h.ID]}):
+                company = {
+                    'id': data[h.ID],
+                    'name': data[h.NAME],
+                    'domain': data[h.DOMAIN],
+                    'year_founded': data[h.YEAR_FOUNDED],
+                    'industry': data[h.INDUSTRY],
+                    'size_range': data[h.SIZE_RANGE],
+                    'locality': data[h.LOCALITY],
+                    'country': data[h.COUNTRY],
+                    'linked_in_url': data[h.LINKEDIN_URL],
+                    'relevant': data[-1]
+                }
+                client.data.companies.insert_one(company)
+            client.data.documents.insert_one(document)
+            client.close()
+        return soup
+    except requests.exceptions.Timeout:
+        error = 'timeout'
+    except requests.exceptions.SSLError:
+        error = 'too many retries'
+    except requests.exceptions.TooManyRedirects:
+        error = 'too many redirects'
+    except requests.exceptions.ConnectionError:
+        error = 'refused connection'
+    client = MongoClient(DB_CONNECTION)
+    failure = {
+        'id': data[h.ID],
+        'endpoint': full_url,
+        'time': datetime.datetime.now(),
+        'error': error
+    }
+    client.data.failures.insert_one(failure)
+    return None
+
+def work_unit(data, is_site_map):
+    if is_site_map:
+        soup = fetch_html(data, data[h.DOMAIN])
+        if soup != None:
+            links = soup.find_all("a")
+            find_about_pages(links, data)
+    else:
+        soup = fetch_html(data, data[h.DOMAIN])
+        if soup != None:
+            links = soup.find_all("a")
+            site_map_targets = find_site_map(links, data)
+            if len(site_map_targets) == 0:
+                find_about_pages(links, data)
+            else:
+                print(site_map_targets)
+                for target in site_map_targets:
+                    data[h.DOMAIN] = target
+                    work_unit(data, True)
+
+def execute_work(data):
+    print('in execute_work...')
+    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+        future_to_url = {
+            executor.submit(work_unit, obj, False): \
+                obj for obj in data
+        }
+
+def load_sam_entities_data(num_enqueues, src_file):
+    data = []
+    counter = 0
+    with open(src_file) as jsonfile:
+        entities = json.load(jsonfile)
+        for entry in entities['domain_agent']:
+            if re.search(h.TARGET_URLS, entry['domain_agent_url']):
+                counter += 1
+                row = [str(counter), entry['domain_agent_name'], \
+                    entry['domain_agent_url'], '', entry['attribute_agent'], \
+                        '', '', '', '', '', '']
+                data.append(row)
+                #if counter > num_enqueues: break
+    return data
+
+def webscrape():
+    data = load_sam_entities_data(num_enqueues=0, src_file='sam_entities.json')
+    print(len(data))
+    execute_work(data)
+
+if __name__ == '__main__':
+    webscrape()
